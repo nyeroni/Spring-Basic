826f14a21b9529521148114184f6140b0ac097c4
nyeroni <nir2y@naver.com>
2023-07-20 00:11:39 +0900

    update

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,12 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-
-
+    private MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); 
-    private final DiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.context.annotation.ComponentScan.*;
 
@@ -15,7 +16,7 @@ public class ComponentFilterAppConfigTest {
     void filterScan(){
         ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
         BeanA beanA = ac.getBean("beanA", BeanA.class);
-        Assertions.assertThat(beanA).isNotNull();
+        assertThat(beanA).isNotNull();
         assertThrows(
                 NoSuchBeanDefinitionException.class,
                 ()-> ac.getBean("beanB", BeanB.class)
@@ -25,8 +26,7 @@ public class ComponentFilterAppConfigTest {
     @Configuration
     @ComponentScan(
             includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
-            excludeFilters = {@Filter(type =FilterType.ANNOTATION, classes =  MyExcludeComponent.class),
-            @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = BeanA.class)}
+            excludeFilters = {@Filter(type =FilterType.ANNOTATION, classes =  MyExcludeComponent.class)}
     )
     static class ComponentFilterAppConfig{
 
