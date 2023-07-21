dba77ca4dd8ae2884296ba51e544020b65565488
nyeroni <nir2y@naver.com>
2023-07-21 15:06:09 +0900

    update

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,12 @@
 package hello.core.lifecycle;
 
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = "+url);
-        connect();
-        call("초기화 연결 메시지");
 
     }
 
@@ -21,11 +20,21 @@ public class NetworkClient {
     }
 
     public void call(String message){
-        System.out.println("call: "+url + " message = "+ message);
+        System.out.println("call: " + url + " message = "+ message);
     }
 
     //서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    public void init() {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+    public void close()  {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
